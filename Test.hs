6536d18d8a8eb90a01c89940c127f383bde72555
Raghu Ugare <raghu.ugare@gmail.com>
2016-10-24 23:54:04 +0530

    Example using fmap/functors! :)

--- Test.hs
+++ Test.hs
@@ -1,4 +1,20 @@
 module Test(main) where
 
+import Data.Maybe(catMaybes)
+
+data Student a = Student {
+  firstName :: String,
+    lastName :: Maybe String,
+    email :: Maybe String,
+    score :: Int
+  }
+
+students = [
+  Student "Raghu" (Just "Ugare") (Just "raghu.ugare@gmail.com") 98,
+  Student "Raj" Nothing (Just "raj@xyz.com") 99
+           ]
+
+emails = catMaybes $ fmap email students
+
 main =
   putStrLn "Hello!"

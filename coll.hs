9ab442021c28eadc74b0512e08c737200483860a
Raghu Ugare <raghu.ugare@gmail.com>
2016-11-17 23:49:29 +0530

    getting ready...

--- coll.hs
+++ coll.hs
@@ -1,8 +1,9 @@
-module coll() where
-
 coll :: (Int,Int)  -> (Int,Int)
 coll (n,count)
-  | n `mod` 2 == 0 = (n/2, count + 1)
+  | n `mod` 2 == 0 = (n `div` 2, count + 1)
   | otherwise = (3*n + 1, count + 1)
 
 -- To be continued for the Collatz sequence.
+main :: IO ()
+main = do
+  print "Hello World!"

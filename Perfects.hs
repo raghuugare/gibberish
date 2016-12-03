c5b67c334b3775b4244d57a22413d9869516071d
Raghu Ugare <raghu.ugare@gmail.com>
2016-12-03 23:51:28 +0530

    Perfect numbers...! :)

---
+++ Perfects.hs
@@ -0,0 +1,31 @@
+{-|
+Module      : Perfects
+Description : Finding Perfect numbers
+Copyright   : (c) Raghu Ugare, 2016
+License     : MIT
+Maintainer  : raghu.ugare@email.com
+Stability   : experimental
+Portability : POSIX
+
+-}
+module Perfects(factors, perfectQ, perfects) where
+
+-- | All the factors of a given +ve integer 'n'.
+--   Written using list comprehension as shown below:
+-- 
+-- @
+-- factors n = [x |  x <- [1..n], n \`mod\` x == 0]
+-- @
+factors :: Int -> [Int]
+factors n = [x |  x <- [1..n], n `mod` x == 0]
+
+sumList :: [Int] -> Int
+sumList = foldr (+) 0
+
+perfectQ :: Int -> Bool
+perfectQ n = sumList (factors n) == 2*n
+
+perfects :: Int -> [Int]
+perfects = filter perfectQ [2..1000000]
+
+

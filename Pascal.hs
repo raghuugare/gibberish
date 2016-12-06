9a8d8ff95efc58c420131663bb4ae45d7825596d
Raghu Ugare <raghu.ugare@gmail.com>
2016-12-06 15:19:24 +0530

    Updated docs!

--- Pascal.hs
+++ Pascal.hs
@@ -7,9 +7,19 @@ Maintainer  : raghu.ugare@email.com
 Stability   : experimental
 Portability : POSIX
 
-Playing with the idea of @Pascal's Triangle@ in Haskell.
+Playing with the idea of @<https://en.wikipedia.org/wiki/Pascal's_triangle Pascal's Triangle>@ in Haskell.
 -}
-module Pascal(row, pascal) where
+module Pascal(
+  -- * Introduction
+  -- $intro
+  -- * Types
+  Row
+  -- * Functions
+  , pascal
+  , row) where
+
+-- $intro
+-- This module has some functions that let us play with Pascal triangles of different sizes
 
 -- | Type definition for a 'row' of Ints.
 type Row = [Int]
@@ -20,6 +30,7 @@ row 0 = [1]
 row n = zipWith (+) (0 : row (n-1)) (row (n-1) ++ [0])
 
 -- | The Pascal's Triangle.
+-- Represented as a list of 'row's
 -- A list of 'rows' formed by the Binomial Coefficients __n-C-r__
 -- 
 -- For example, Pascal's triangle with @n=3@ looks like below:

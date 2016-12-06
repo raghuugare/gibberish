872ce5f63e24be38f08d5ae56074476d444cc857
Raghu Ugare <raghu.ugare@gmail.com>
2016-12-06 15:25:29 +0530

    Move doc around a bit.

--- Pascal.hs
+++ Pascal.hs
@@ -20,19 +20,8 @@ module Pascal(
 
 -- $intro
 -- This module has some functions that let us play with Pascal triangles of different sizes
-
--- | Type definition for a 'row' of Ints.
-type Row = [Int]
-
--- | Each row of the Pascal's Triangle.
-row :: Int -> Row
-row 0 = [1]
-row n = zipWith (+) (0 : row (n-1)) (row (n-1) ++ [0])
-
--- | The Pascal's Triangle.
--- Represented as a list of 'row's
--- A list of 'rows' formed by the Binomial Coefficients __n-C-r__
--- 
+-- Each row @r@ corresponds to the co-efficients of the expansion __@(a + b)^r@__
+--
 -- For example, Pascal's triangle with @n=3@ looks like below:
 -- 
 -- @
@@ -44,6 +33,17 @@ row n = zipWith (+) (0 : row (n-1)) (row (n-1) ++ [0])
 --
 -- 1 3 3 1
 -- @
+
+-- | Type definition for a 'row' of Ints.
+type Row = [Int]
+
+-- | Each row of the Pascal's Triangle.
+row :: Int -> Row
+row 0 = [1]
+row n = zipWith (+) (0 : row (n-1)) (row (n-1) ++ [0])
+
+-- | The Pascal's Triangle.
+-- Represented as a list of 'row's
 pascal :: Int -> [Row]
 pascal n = map row [0..n]
 

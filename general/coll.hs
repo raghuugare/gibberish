7d521c474301c7b7c981ae2a17131b574da64e06
Raghu Ugare <raghu.ugare@gmail.com>
2016-12-16 23:52:48 +0530

    Add a function for Collatz sequence using until! :)

--- general/coll.hs
+++ general/coll.hs
@@ -3,6 +3,9 @@ coll (n,count)
   | n `mod` 2 == 0 = (n `div` 2, count + 1)
   | otherwise = (3*n + 1, count + 1)
 
+f :: Integer -> Integer
+f  = until (==0) (\x -> if even x then x `div` 2 else (if x==1 then 0 else 3*x+1))
+
 -- To be continued for the Collatz sequence.
 main :: IO ()
 main = do

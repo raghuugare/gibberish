684c51bda3ed43b1c5fa1a034e1cdde765ff5179
Raghu Ugare <raghu.ugare@gmail.com>
2017-02-09 21:38:50 +0530

    Fibonacci series using List comprehension.

--- general/fib.hs
+++ general/fib.hs
@@ -4,5 +4,9 @@ fib 0 = 1
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
+-- A recursive definition of the Fibonacci series using List Comprehension!
+fibs :: [Int]
+fibs = 1 : 1 : [ a + b | (a,b) <- zip fibs (tail fibs) ]
+
 main :: IO ()
 main = print (fib 10)

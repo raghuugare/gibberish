36c388bf5581949cff756a8379f9e7c3557061f5
Raghu Ugare <raghu.ugare@gmail.com>
2017-02-11 23:27:47 +0530

    Sumtorial too!

--- basics/Length.hs
+++ basics/Length.hs
@@ -5,5 +5,9 @@ factorial :: Int -> Int
 factorial 1 = 1
 factorial n = n * factorial (n-1)
 
+sumtorial :: Int -> Int
+sumtorial 1 = 1
+sumtorial n = n + sumtorial (n-1)
+
 factorial' :: Int -> Int
 factorial' n = product [1..n]

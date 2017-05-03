8a2ca64bfba0b5712af65f0adea1f638aabb2cc9
Raghu Ugare <raghu.ugare@gmail.com>
2017-05-03 23:54:20 +0530

    Removed un-related functions.

--- basics/Length.hs
+++ basics/Length.hs
@@ -1,13 +1,3 @@
+-- Length of a List using recursion.
 len [] = 0
 len (_:xs) = 1 + len xs
-
-factorial :: Int -> Int
-factorial 1 = 1
-factorial n = n * factorial (n-1)
-
-sumtorial :: Int -> Int
-sumtorial 1 = 1
-sumtorial n = n + sumtorial (n-1)
-
-factorial' :: Int -> Int
-factorial' n = product [1..n]
